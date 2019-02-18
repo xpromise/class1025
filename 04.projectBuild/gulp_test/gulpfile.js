52fa47f340e3eab3fd987fa8cc2013131a01d51d
xiongjian <webjsforyou@163.com>
2019-02-18 11:09:19 +0800

    '完成js文件处理'

--- 04.projectBuild/gulp_test/gulpfile.js
+++ 04.projectBuild/gulp_test/gulpfile.js
@@ -6,8 +6,27 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const browserify = require('gulp-browserify');
 const rename = require("gulp-rename");
+const eslint = require('gulp-eslint');
+const uglify = require('gulp-uglify');
 
 //注册任务
+//语法检查：eslint jshint jslint
+//使用 "extends": "eslint:recommended" 来启用推荐的规则
+/*
+  package.json中修改
+    "eslintConfig": {
+      "parserOptions": {
+        "ecmaVersion": 7,
+        "sourceType": "module"
+      }
+    }
+ */
+gulp.task('eslint', () => {
+  return gulp.src(['src/js/*.js'])
+    .pipe(eslint())  //语法检查
+    .pipe(eslint.format())  //将检查出来的语法错误，打印到控制台中
+});
+
 gulp.task('babel', function () {
   //当前任务要执行的回调函数
   return gulp.src('src/js/*.js')      //将要处理的js文件引入进来
@@ -23,3 +42,15 @@ gulp.task('browserify', function() {
     .pipe(rename('built.js'))
     .pipe(gulp.dest('./build/js'))
 });
+
+//压缩js
+gulp.task('uglify', function() {
+  return gulp.src('build/js/built.js')
+    .pipe(uglify())  //压缩js代码
+    .pipe(rename('dist.min.js'))
+    .pipe(gulp.dest('./dist/js'))
+});
+
+//通过gulp js启动， 实际执行的是'babel', 'browserify'任务
+gulp.task('js', gulp.series('eslint', 'babel', 'browserify', 'uglify'))  //顺序执行 - 同一时间只能执行一个任务
+// gulp.task('js', gulp.parallel('babel', 'browserify'))  //并行执行 - 同一时间执行多个任务
